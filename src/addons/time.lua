662e61e4b2bbff9aff7b8d40baec1af6f7d443c8
yaroslav_polishchuk <y.polishchuk05@gmail.com>
2026-04-25 18:39:53 +0200

    fix: I've already fixed this a thousand times. Whoever's reading this, don't ask why I'm editing every single change, okay?

--- src/addons/time.lua
+++ src/addons/time.lua
@@ -1,8 +1,10 @@
 local time = {}
 
 function time.get()
-  local now = os.date("*t")
-  return string.format("%02d:%02d", now.hour, now.min)
+  local h = os.time()
+  local hh = math.floor(h) % 24
+  local mm = math.floor((h % 1) * 60)
+  return string.format("%02d:%02d:%02d", hh, mm)
 end
 
 return time
