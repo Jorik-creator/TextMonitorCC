f41b2e349034fcb537b0c158dd4763270068c569
yaroslav_polishchuk <y.polishchuk05@gmail.com>
2026-04-25 18:36:02 +0200

    fix: use os.date to get correct system time

--- src/addons/time.lua
+++ src/addons/time.lua
@@ -1,10 +1,8 @@
 local time = {}
 
 function time.get()
-  local h = os.time()
-  local hh = math.floor(h / 3600) % 24
-  local mm = math.floor(h / 60) % 60
-  return string.format("%02d:%02d", hh, mm)
+  local now = os.date("*t")
+  return string.format("%02d:%02d", now.hour, now.min)
 end
 
 return time
