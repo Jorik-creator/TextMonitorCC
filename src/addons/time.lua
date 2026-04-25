e9b6bd12df15db5cc1db209e3967b41550556f6c
yaroslav_polishchuk <y.polishchuk05@gmail.com>
2026-04-25 18:41:41 +0200

    fix: addon time....

--- src/addons/time.lua
+++ src/addons/time.lua
@@ -4,7 +4,7 @@ function time.get()
   local h = os.time()
   local hh = math.floor(h) % 24
   local mm = math.floor((h % 1) * 60)
-  return string.format("%02d:%02d:%02d", hh, mm)
+  return string.format("%02d:%02d", hh, mm)
 end
 
 return time
